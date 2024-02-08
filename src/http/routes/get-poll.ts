b765999c599291c9e62cd853f0100e455b1961c0
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-08 09:39:27 -0300

    feat: save vote on redis and show score

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import z from "zod";
 
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
   app.get("/polls/:pollId", async (request, reply) => {
@@ -33,6 +34,28 @@ export async function getPoll(app: FastifyInstance) {
       });
     }
 
-    return reply.send({ poll });
+    const result = await redis.zrange(pollId, 0, -1, "WITHSCORES");
+
+    const votes = result.reduce((prev, cur, index) => {
+      if (index % 2 === 0) {
+        const score = result[index + 1];
+
+        Object.assign(prev, { [cur]: Number(score) });
+      }
+
+      return prev;
+    }, {} as Record<string, number>);
+
+    return reply.send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map(item => ({
+          id: item.id,
+          title: item.title,
+          score: item.id in votes ? votes[item.id] : 0
+        })),
+      }
+    });
   });
 }
